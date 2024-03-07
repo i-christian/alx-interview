a5fb7cb905703b1a544bf73bc2d06c1af5043ea2
Christian <111076892+i-christian@users.noreply.github.com>
2024-03-07 21:45:16 +0200

    update solution

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,24 +1,19 @@
 def pascal_triangle(n):
     """
-    Generate Pascal's Triangle up to the nth row
+    Generate Pascal's Triangle up to the nth row using dynamic programming
     """
     if n <= 0:
         return []
 
-    # Memoization dictionary to store computed values of combinations
-    memo = {}
+    triangle = [[1]]
 
-    def combination(n, k):
-        """
-        Calculate the binomial coefficient C(n, k) using memoization
-        """
-        if k == 0 or k == n:
-            return 1
-        if (n, k) in memo:
-            return memo[(n, k)]
-        result = combination(n - 1, k - 1) + combination(n - 1, k)
-        memo[(n, k)] = result
-        return result
+    for i in range(1, n):
+        row = [1]
+        for j in range(1, i):
+            left_val = triangle[i - 1][j - 1]
+            right_val = triangle[i - 1][j]
+            row.append(left_val + right_val)
+        row.append(1)  # Adding the last element for each row
+        triangle.append(row)
 
-    triangle = [[combination(i, j) for j in range(i + 1)] for i in range(n)]
     return triangle
