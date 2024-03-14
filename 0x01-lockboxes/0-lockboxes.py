af4a65e7df506b0ce82f212ee1fa129f4c21901a
Christian <111076892+i-christian@users.noreply.github.com>
2024-03-14 22:32:12 +0200

    update solution

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-from collections import deque
 """
 Using BFS algorithm
 """
@@ -10,19 +9,18 @@ def canUnlockAll(boxes):
     Determines if all the boxes can be opened.
 
     Args:
-    - boxes: A list of lists.
+    - boxes (list of lists): A list of lists where each inner list represents a box containing keys to other boxes.
 
     Returns:
     - bool: True if all boxes can be opened, False otherwise.
     """
-    visited = set()
-    queue = deque([0])
-
-    while queue:
-        current_box = queue.popleft()
+    def dfs(current_box, visited):
         visited.add(current_box)
         for key in boxes[current_box]:
             if key not in visited:
-                queue.append(key)
+                dfs(key, visited)
+
+    visited = set()
+    dfs(0, visited)
 
     return len(visited) == len(boxes)
